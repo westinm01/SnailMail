6b930cb126d8f1478e30593365f4c94e671f180e
Wes <wmont012@ucr.edu>
2022-09-10 10:44:56 -0700

    Almost done with camera bounds

--- Snail Mail/Assets/Scripts/Gameplay/PlayerMovement.cs
+++ Snail Mail/Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -12,11 +12,16 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 20.0f;
     //private Animator anim;
     
+    public float leftBound = -8;
+    public float rightBound = 8;
+    public float topBound = 4;
+    public float bottomBound = -4;
 
     void Start ()
     {
     body = GetComponent<Rigidbody2D>();
     //anim = GetComponent<Animator>();
+    
     }
 
     void Update()
@@ -38,6 +43,53 @@ public class PlayerMovement : MonoBehaviour
             anim.SetBool("isWalking", false);
         }
         */
+        Debug.Log(horizontal + ", " + vertical);
+        if (this.transform.position.x < rightBound && this.transform.position.x > leftBound && this.transform.position.y < topBound && this.transform.position.y > bottomBound)
+        {
+            Move();
+        }
+        //corner cases
+        else if (this.transform.position.x >= rightBound && horizontal <= 0 && this.transform.position.y >= topBound && vertical <= 0)
+        {
+            Move();
+        }
+        else if (this.transform.position.x >= rightBound && horizontal <= 0 && this.transform.position.y <= bottomBound && vertical >= 0)
+        {
+            Move();
+        }
+        else if (this.transform.position.x <= leftBound && horizontal >= 0 && this.transform.position.y >= topBound && vertical <= 0)
+        {
+            Move();
+        }
+        else if (this.transform.position.x <= leftBound && horizontal >= 0 && this.transform.position.y <= bottomBound && vertical >= 0)
+        {
+            
+            Move();
+        }
+        //single plane cases
+        else if (this.transform.position.x >= rightBound && horizontal <= 0 && InYBounds()){
+            Move();
+        }
+        else if (this.transform.position.x <= leftBound && horizontal >= 0 && InYBounds())
+        {
+            Move();
+        }
+        else if (this.transform.position.y >= topBound && vertical <= 0 && InXBounds())
+        {
+            Move();
+        }
+        else if (this.transform.position.y <= bottomBound && vertical >= 0 && InXBounds())
+        {
+            Move();
+        }
+        else{
+            body.velocity = new Vector2(0,0);//get background move velocity?
+        }
+        
+
+    }
+
+    void Move(){
         if (horizontal != 0 && vertical != 0) // Check for diagonal movement and slow it
         {
             body.velocity = new Vector2(horizontal * runSpeed * 0.7f, vertical * runSpeed * 0.7f); 
@@ -46,6 +98,15 @@ public class PlayerMovement : MonoBehaviour
         {
             body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
         }
+    }
 
+    bool InXBounds()
+    {
+        return this.transform.position.x < rightBound && this.transform.position.y > leftBound;
+    }
+
+    bool InYBounds()
+    {
+        return this.transform.position.y < topBound && this.transform.position.y > bottomBound;
     }
 }
