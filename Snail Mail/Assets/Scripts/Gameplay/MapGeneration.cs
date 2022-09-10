8af8fd7f12569ffe38b05886fdc7f74a9b43b1d5
Wes <wmont012@ucr.edu>
2022-09-10 11:51:26 -0700

    Map Generation done!

---
+++ Snail Mail/Assets/Scripts/Gameplay/MapGeneration.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneration : MonoBehaviour
+{
+    public float timer;
+    public float startOffset;
+    
+    public GameObject backgroundObject;
+    private float timePassed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timePassed = startOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if(timePassed >= timer)
+        {   GameObject newMap = Instantiate(backgroundObject, this.transform, false);
+            newMap.transform.position = new Vector3(19.0f, 0, 10);
+            timePassed = 0;
+        }
+    }
+}
