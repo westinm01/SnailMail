236ad909879acbebe242efa0d4836cba58b6d20a
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2022-09-10 11:58:42 -0700

    Quick Commit

---
+++ Snail Mail/Assets/Scripts/Gameplay/GameStateHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateHandler : MonoBehaviour
+{
+    [SerializeField] GameObject winScreen;
+    [SerializeField] GameObject loseScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
